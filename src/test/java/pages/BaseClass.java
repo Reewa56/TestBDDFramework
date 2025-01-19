867b53b7a61957c0eed84f09f282ed2da033f392
Reewa <reewa56@gmail.com>
2025-01-19 23:25:42 +0000

    code changes added chromeoptions

--- src/test/java/pages/BaseClass.java
+++ src/test/java/pages/BaseClass.java
@@ -2,12 +2,16 @@ package pages;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BaseClass {
 
     public WebDriver driver;
 
     public WebDriver Initializedriver(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
         driver = new ChromeDriver();
         return driver;
     }
